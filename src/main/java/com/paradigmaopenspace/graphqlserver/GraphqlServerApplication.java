a511e917557c02dc97bb7d2eb1917b05d5d5f202
JESBA8F <jestravis@paradigmadigital.com>
2022-09-16 12:26:41 +0200

    Primera versión funcionando

---
+++ src/main/java/com/paradigmaopenspace/graphqlserver/GraphqlServerApplication.java
@@ -0,0 +1,44 @@
+package com.paradigmaopenspace.graphqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+public class GraphqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlServerApplication.class, args);
+	}
+
+}
+
+@Controller
+class GraphqlController{
+	AtomicLong idCounter=new AtomicLong();
+	List<Artista> bbdd=List.of(
+			new Artista(idCounter.incrementAndGet(),"Levstein","Videoarte"),
+			new Artista(idCounter.incrementAndGet(),"Casile","Performance"));
+
+	@QueryMapping
+	Flux<Artista> artistas(){
+		return Flux.fromIterable(bbdd);
+	}
+}
+
+record Artista (Long id, String apellido, String estilo){}
---
+++ src/test/java/com/paradigmaopenspace/graphqlserver/GraphqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigmaopenspace.graphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
