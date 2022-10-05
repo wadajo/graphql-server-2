fee1a658f6010dcd064b784de09a5991ccd2dc7b
jestravis <jestravis@paradigmadigital.com>
2022-10-05 10:13:00 +0200

    Primer test exitoso con @GraphQlTest

--- src/test/java/com/paradigmaopenspace/graphqlserver/GraphqlServerApplicationTests.java
+++ src/test/java/com/paradigmaopenspace/graphqlserver/GraphqlServerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.paradigmaopenspace.graphqlserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
 
-@SpringBootTest
+import java.util.List;
+
+@GraphQlTest(GraphqlController.class)
 class GraphqlServerApplicationTests {
 
+	@Autowired
+	private GraphQlTester graphQlTester;
+
 	@Test
-	void contextLoads() {
+	void apellidos(){
+		this.graphQlTester
+				.documentName("artistas_test1")
+				.execute()
+				.path("artistas")
+				.entity(List.class)
+				.matches(lista->lista.size()==3);
 	}
 
 }
